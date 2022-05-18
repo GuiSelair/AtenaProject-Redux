aca256cf415094e1604eaa3dc98d0026aa33e4e9
Guilherme Selair <guilherme.lima@getrak.com.br>
2022-05-18 11:49:43 -0300

    Update: Added increment exits item quantity

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -8,19 +8,27 @@ const INITIAL_STATE: ICartState = {
 }
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-	switch(action.type) {
-		case "ADD_PRODUCT_TO_CART": 
-			const { product } = action.payload;
+	return produce(state, draft => {
+		switch(action.type) {
+			case "ADD_PRODUCT_TO_CART": 
+				const { product } = action.payload;
 
-			return produce(state, draft => {
-				draft.items.push({
-					product,
-					quantity: 1
-				})
-			})	
-		default:
-			return state;
-	}
+				const productInCartIndex = draft.items.findIndex(item => item.product.id === product.id);
+
+				if (productInCartIndex >= 0) {
+					draft.items[productInCartIndex].quantity += 1;
+				} else {
+					draft.items.push({
+						product,
+						quantity: 1
+					})
+				}
+				
+				break;
+			default:
+				return state;
+			}
+	})	
 
 } 
 
