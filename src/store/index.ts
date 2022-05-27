c37ca3e8ca9a0db588efb19a6dc3062109787612
Guilherme Selair <guilherme.lima@getrak.com.br>
2022-05-27 18:10:04 -0300

    Feat: Install and configure redux saga

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { ICartState } from "./modules/cart/types";
+import createSagaMiddleware from "redux-saga";
 
+import { ICartState } from "./modules/cart/types";
 import rootReducer from "./modules/rootReducer";
+import rootSaga from "./modules/rootSaga";
 
 export interface IState {
 	cart: ICartState;
 }
 
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
 const store = configureStore({
-    reducer: rootReducer
+    reducer: rootReducer,
+		middleware: middlewares
 })
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
 // Infer the `RootState` and `AppDispatch` types from the store itself
